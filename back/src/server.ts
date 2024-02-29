c723599d7940d46fb6dda24dc9d8ac0e4d820459
nicolaserd <530nicolas@gmail.com>
2024-02-29 09:45:20 -0500

    h3

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,15 @@
 import express = require('express');
 import userRouter from './routes/userRouter';
 import appointmentRouter from './routes/appointmentRouter';
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
+app.use(morgan("dev"));
+
 app.use("/users", userRouter);
 app.use("/", appointmentRouter);
 
