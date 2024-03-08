c62216857f705294a5e2e69afceef614adc1f76d
nicolaserd <530nicolas@gmail.com>
2024-03-07 19:25:21 -0500

    h9

--- front/src/components/CardAppointment.jsx
+++ front/src/components/CardAppointment.jsx
@@ -12,14 +12,14 @@ const CardAppointment = ({turno:{date,time,status,user,id}}) => {
             <div className="card__front">
               <div className="body__card_front">
                 <div className="bg"></div>
-                <h1>CITA NÚMERO {id}</h1>
+                <h1 style={status === "active" ? { color: "#2fff00" } : { color: "#ff000e" }}>CITA NÚMERO {id}</h1>
               </div>
             </div>
             <div className="card__back">
               <div className="body__card_back">
                 <h1>Usuario : {user.name} </h1>
                 <p>
-                  Su cita  esta en estado {status} para el dia {date} a la hora {time}
+                  Su cita  esta en estado   <spam style={status === "active" ? { color: "green" } : { color: "red" }}>{status === "active" ? "activa" : "cancelada"} </spam> para el dia {date} a la hora {time}
                 </p>
                 <input type="button" value="Cancelar cita" />
               </div>
